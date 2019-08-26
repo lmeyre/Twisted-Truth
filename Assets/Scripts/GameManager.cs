23d617321ab09316c8cbb0e128e54de1b5b6297c
Meyre <p>
2019-08-26 11:08:35 +0200

    update menu et polish de la veille

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject menu;
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+            menu.SetActive(true);
+    }
+}
